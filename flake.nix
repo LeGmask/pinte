7be68e35beb58d6017b804bcbecb35ed19473af5
Louis Thevenet <louis.tvnt@gmail.com>
2024-02-12 08:59:08 +0100

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A Nix flake dev environment for N7 assignements in Java";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Nix
+            nil
+            alejandra
+
+            # Typst
+            typst
+            typst-lsp
+            #typst-fmt
+
+            # Java
+            jdk21
+          ];
+        };
+      };
+    });
+}
