901436d7ad7509ca5c0357b4da874cb8cafed473
Louis Thevenet <louis.tvnt@gmail.com>
2024-04-22 13:18:48 +0200

    test: tests for CanvasPolygon

--- src/test/java/org/pinte/models/CanvasObjects/CanvasPolygonTest.java
+++ src/test/java/org/pinte/models/CanvasObjects/CanvasPolygonTest.java
@@ -1,18 +1,77 @@
 package org.pinte.models.CanvasObjects;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.pinte.models.Utils.CanvasObjectParser;
+
+import javafx.geometry.Point2D;
+import javafx.scene.shape.Polygon;
 
 /**
-  * Classe de test de CanvasPolygon
-  *
-  * @author	---
-  */
+ * Classe de test de CanvasEllipse
+ *
+ * @author Louis Thevenet
+ */
 
 public class CanvasPolygonTest {
 
-	// TODO : Tests unitaires pour CanvasPolygon
+    private Point2D[] points;
+    CanvasColor fillColor, strokeColor;
+    String SVGString;
 
+    @BeforeEach
+    public void setUp() {
+        points = new Point2D[3];
+        points[0] = new Point2D(10, 10);
+        points[1] = new Point2D(50, 50);
+        points[2] = new Point2D(90, 10);
 
+        fillColor = new CanvasColor(0, 255, 0);
+        strokeColor = new CanvasColor(0, 0, 255);
+    }
+
+    @Test
+    public void testSVGString() {
+        CanvasPolygon polygon = new CanvasPolygon(points, fillColor, strokeColor);
+        String svgString = polygon.toSVG();
+        Point2D[] parsedPoints = CanvasObjectParser.parsePoints(svgString);
+        CanvasColor fill = new CanvasColor(CanvasObjectParser.parseKeyword("fill", svgString));
+        CanvasColor stroke = new CanvasColor(CanvasObjectParser.parseKeyword("stroke", svgString));
+
+        assertEquals(parsedPoints.length, points.length);
+
+        for (int i = 0; i < points.length; ++i) {
+            assertTrue(Arrays.asList(points).contains(parsedPoints[i]));
+        }
+
+        Assertions.assertEquals(fill.asHex(), fillColor.asHex());
+        Assertions.assertEquals(stroke.asHex(), strokeColor.asHex());
+    }
+
+    @Test
+    public void testRender() {
+        CanvasPolygon polygon = new CanvasPolygon(points, fillColor, strokeColor);
+
+        Polygon s = (Polygon) polygon.render();
+        Polygon s2 = new Polygon();
+        for (Point2D point : points) {
+            s2.getPoints().addAll(point.getX(), point.getY());
+        }
+        s2.setFill(fillColor.toPaintColor());
+        s2.setStroke(strokeColor.toPaintColor());
+
+        Assertions.assertEquals(s.getPoints().size(), s2.getPoints().size());
+        for (int i = 0; i < points.length; ++i) {
+            Assertions.assertEquals(s.getPoints().get(i), s2.getPoints().get(i));
+        }
+        Assertions.assertEquals(s.getFill(), s2.getFill());
+        Assertions.assertEquals(s.getStroke(), s2.getStroke());
+
+    }
 }
