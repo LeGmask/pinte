2d2bb4bc59a572f30789e77d1c90f82e68aaa1f5
Hdvbxh7 <tom.gutierrez@etu.inp-n7.fr>
2024-04-28 18:15:30 +0200

    feat(enregistrer): creation du bouton et recuperation de l'url

--- src/main/java/org/pinte/controllers/MenuBar.java
+++ src/main/java/org/pinte/controllers/MenuBar.java
@@ -3,16 +3,18 @@ package org.pinte.controllers;
 import javafx.scene.control.Menu;
 import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 public class MenuBar {
 	public javafx.scene.control.MenuBar menuBar;
-
+	
 	// on create
 	public void initialize() {
-		Menu menu = new Menu("Menu 1");
+		Menu menu = new Menu("Fichier");
 
-		RadioMenuItem choice1Item = new RadioMenuItem("Choice 1");
-		RadioMenuItem choice2Item = new RadioMenuItem("Choice 2");
+		RadioMenuItem choice1Item = new RadioMenuItem("enregistrer sous");
+		RadioMenuItem choice2Item = new RadioMenuItem("enregistrer");
 		RadioMenuItem choice3Item = new RadioMenuItem("Choice 3");
 
 		ToggleGroup toggleGroup = new ToggleGroup();
@@ -25,5 +27,20 @@ public class MenuBar {
 		menu.getItems().add(choice3Item);
 
 		menuBar.getMenus().add(menu);
+		
+		choice1Item.setOnAction(event->{enregistrerss();});
+		choice1Item.setOnAction(event->{enregistrer();});
 	}
+	
+	public void enregistrerss(){
+		System.out.println("enregistrer");
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		directoryChooser.setTitle("Select Project Location");
+		System.out.println(directoryChooser.showDialog(null).getAbsolutePath());
+	}
+
+	public void enregistrer(){
+		
+	}
+
 }
