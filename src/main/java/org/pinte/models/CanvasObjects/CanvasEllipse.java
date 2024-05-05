a4cecad050ff3a90bc859e5a1344cbde35e442e1
Louis Thevenet <louis.tvnt@gmail.com>
2024-05-05 13:40:24 +0200

    fix: stroking no longer drawn

--- src/main/java/org/pinte/models/CanvasObjects/CanvasEllipse.java
+++ src/main/java/org/pinte/models/CanvasObjects/CanvasEllipse.java
@@ -12,97 +12,97 @@ import java.util.Hashtable;
  * Ellipse canvas object
  */
 public class CanvasEllipse extends CanvasObject {
-	Point2D center;
-	double rx, ry;
+    Point2D center;
+    double rx, ry;
 
-	/**
-	 * Constructor for an ellipse object on the canvas.
-	 *
-	 * @param center      center of the ellipse
-	 * @param rx          radius in x direction
-	 * @param ry          radius in y direction
-	 * @param fillColor   fill color of the ellipse
-	 * @param strokeColor stroke color of the ellipse
-	 */
-	public CanvasEllipse(Point2D center, double rx, double ry, CanvasColor fillColor, CanvasColor strokeColor) {
-		super(fillColor, strokeColor);
-		this.center = center;
-		this.rx = rx;
-		this.ry = ry;
-	}
+    /**
+     * Constructor for an ellipse object on the canvas.
+     *
+     * @param center      center of the ellipse
+     * @param rx          radius in x direction
+     * @param ry          radius in y direction
+     * @param fillColor   fill color of the ellipse
+     * @param strokeColor stroke color of the ellipse
+     */
+    public CanvasEllipse(Point2D center, double rx, double ry, CanvasColor fillColor, CanvasColor strokeColor) {
+        super(fillColor, strokeColor);
+        this.center = center;
+        this.rx = rx;
+        this.ry = ry;
+    }
 
-	/**
-	 * Constructor for a circle object on the canvas.
-	 *
-	 * @param center      center of the circle
-	 * @param r           radius of the circle
-	 * @param fillColor   fill color of the circle
-	 * @param strokeColor stroke color of the circle
-	 */
-	public CanvasEllipse(Point2D center, double r, CanvasColor fillColor, CanvasColor strokeColor) {
-		super(fillColor, strokeColor);
-		this.center = center;
-		this.rx = r;
-		this.ry = r;
-	}
+    /**
+     * Constructor for a circle object on the canvas.
+     *
+     * @param center      center of the circle
+     * @param r           radius of the circle
+     * @param fillColor   fill color of the circle
+     * @param strokeColor stroke color of the circle
+     */
+    public CanvasEllipse(Point2D center, double r, CanvasColor fillColor, CanvasColor strokeColor) {
+        super(fillColor, strokeColor);
+        this.center = center;
+        this.rx = r;
+        this.ry = r;
+    }
 
-	/**
-	 * Constructor for an ellipse object on the canvas. Creates an ellipse from two
-	 * points on the canvas.
-	 *
-	 * @param a           first point
-	 * @param b           second point
-	 * @param fillColor   fill color of the ellipse
-	 * @param strokeColor stroke color of the ellipse
-	 */
-	public CanvasEllipse(Point2D a, Point2D b, CanvasColor fillColor, CanvasColor strokeColor) {
-		super(fillColor, strokeColor);
-		this.center = a.midpoint(b);
-		this.rx = Math.abs(a.getX() - this.center.getX());
-		this.ry = Math.abs(a.getY() - this.center.getY());
-	}
+    /**
+     * Constructor for an ellipse object on the canvas. Creates an ellipse from two
+     * points on the canvas.
+     *
+     * @param a           first point
+     * @param b           second point
+     * @param fillColor   fill color of the ellipse
+     * @param strokeColor stroke color of the ellipse
+     */
+    public CanvasEllipse(Point2D a, Point2D b, CanvasColor fillColor, CanvasColor strokeColor) {
+        super(fillColor, strokeColor);
+        this.center = a.midpoint(b);
+        this.rx = Math.abs(a.getX() - this.center.getX());
+        this.ry = Math.abs(a.getY() - this.center.getY());
+    }
 
+    /**
+     * Creates an ellipse object from an SVG string.
+     *
+     * @param args SVG string
+     * @return CanvasEllipse object
+     */
+    public static CanvasEllipse createFromSVG(String args) {
+        double cx = Double.parseDouble(CanvasObjectParser.parseKeyword("cx", args));
+        double cy = Double.parseDouble(CanvasObjectParser.parseKeyword("cy", args));
 
-	/**
-	 * Creates an ellipse object from an SVG string.
-	 *
-	 * @param args SVG string
-	 * @return CanvasEllipse object
-	 */
-	public static CanvasEllipse createFromSVG(String args) {
-		double cx = Double.parseDouble(CanvasObjectParser.parseKeyword("cx", args));
-		double cy = Double.parseDouble(CanvasObjectParser.parseKeyword("cy", args));
+        double rx = Double.parseDouble(CanvasObjectParser.parseKeyword("rx", args));
+        double ry = Double.parseDouble(CanvasObjectParser.parseKeyword("ry", args));
 
-		double rx = Double.parseDouble(CanvasObjectParser.parseKeyword("rx", args));
-		double ry = Double.parseDouble(CanvasObjectParser.parseKeyword("ry", args));
+        CanvasColor fillColor = new CanvasColor(CanvasObjectParser.parseKeyword("fill", args));
+        CanvasColor strokeColor = new CanvasColor(CanvasObjectParser.parseKeyword("stroke", args));
+        return new CanvasEllipse(new Point2D(cx, cy), rx, ry, fillColor, strokeColor);
+    }
 
-		CanvasColor fillColor = new CanvasColor(CanvasObjectParser.parseKeyword("fill", args));
-		CanvasColor strokeColor = new CanvasColor(CanvasObjectParser.parseKeyword("stroke", args));
-		return new CanvasEllipse(new Point2D(cx, cy), rx, ry, fillColor, strokeColor);
-	}
+    /**
+     * Renders the ellipse on the canvas
+     */
+    public void render() {
+        this.gc.setFill(this.fillColor.toPaintColor());
+        this.gc.setStroke(this.strokeColor.toPaintColor());
+        this.gc.fillOval(center.getX(), center.getY(), rx, ry);
+        this.gc.strokeOval(center.getX(), center.getY(), rx, ry);
+    }
 
-	/**
-	 * Renders the ellipse on the canvas
-	 */
-	public void render() {
-		this.gc.setFill(this.fillColor.toPaintColor());
-		this.gc.setStroke(this.strokeColor.toPaintColor());
-		this.gc.fillOval(center.getX(), center.getY(), rx, ry);
-	}
-
-	/**
-	 * Exports the ellipse to SVG format
-	 *
-	 * @return the ellipse in SVG format
-	 */
-	public String toSVG() {
-		Dictionary<String, String> d = new Hashtable<>();
-		d.put("cx", Double.toString(this.center.getX()));
-		d.put("cy", Double.toString(this.center.getY()));
-		d.put("rx", Double.toString(this.rx));
-		d.put("ry", Double.toString(this.ry));
-		d.put("fill", this.fillColor.asHex());
-		d.put("stroke", this.strokeColor.asHex());
-		return toSVG("ellipse", d);
-	}
+    /**
+     * Exports the ellipse to SVG format
+     *
+     * @return the ellipse in SVG format
+     */
+    public String toSVG() {
+        Dictionary<String, String> d = new Hashtable<>();
+        d.put("cx", Double.toString(this.center.getX()));
+        d.put("cy", Double.toString(this.center.getY()));
+        d.put("rx", Double.toString(this.rx));
+        d.put("ry", Double.toString(this.ry));
+        d.put("fill", this.fillColor.asHex());
+        d.put("stroke", this.strokeColor.asHex());
+        return toSVG("ellipse", d);
+    }
 }
