10f53b587bf5e3c77608a3361d242b54d7e86c7c
Louis Thevenet <louis.tvnt@gmail.com>
2024-05-16 11:43:43 +0200

    feat: add support for opacity in CanvasColor

--- src/test/java/org/pinte/models/CanvasObjects/CanvasColorTest.java
+++ src/test/java/org/pinte/models/CanvasObjects/CanvasColorTest.java
@@ -9,102 +9,103 @@ import org.junit.jupiter.api.Test;
  */
 public class CanvasColorTest {
 
-	// CanvasColor(r, g, b)
-	@Test
-	public void testNormalUseCanvasColorRGB() {
-		CanvasColor color = new CanvasColor(20, 50, 70);
-		Assertions.assertEquals("#143246", color.asHex());
-	}
+    // CanvasColor(r, g, b)
+    @Test
+    public void testNormalUseCanvasColorRGB() {
+        CanvasColor color = new CanvasColor(20, 50, 70);
+        Assertions.assertEquals("#143246", color.asHex());
+    }
 
-	@Test
-	public void testNormalUseCanvasColorRGBWhite() {
-		CanvasColor color = new CanvasColor(255, 255, 255);
-		Assertions.assertEquals("#FFFFFF", color.asHex());
-	}
+    @Test
+    public void testNormalUseCanvasColorRGBWhite() {
+        CanvasColor color = new CanvasColor(255, 255, 255);
+        Assertions.assertEquals("#FFFFFF", color.asHex());
+    }
 
-	@Test
-	public void testNormalUseCanvasColorRGBBlack() {
-		CanvasColor color = new CanvasColor(0, 0, 0);
-		Assertions.assertEquals("#000000", color.asHex());
-	}
+    @Test
+    public void testNormalUseCanvasColorRGBBlack() {
+        CanvasColor color = new CanvasColor(0, 0, 0);
+        Assertions.assertEquals("#000000", color.asHex());
+    }
 
-	@Test
-	public void testNegativeRCanvasColorRGBValueThrowsIllegalArgumentException() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(-30, 10, 70));
-	}
+    @Test
+    public void testNegativeRCanvasColorRGBValueThrowsIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(-30, 10, 70));
+    }
 
-	@Test
-	public void testNegativeGValueCanvasColorRGBThrowsIllegalArgumentException() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(30, -10, 70));
-	}
+    @Test
+    public void testNegativeGValueCanvasColorRGBThrowsIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(30, -10, 70));
+    }
 
-	@Test
-	public void testNegativeBValueCanvasColorRGBThrowsIllegalArgumentException() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(30, 10, -70));
-	}
+    @Test
+    public void testNegativeBValueCanvasColorRGBThrowsIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(30, 10, -70));
+    }
 
-	@Test
-	public void testAbove255RValueCanvasColorRGBThrowsIllegalArgumentException() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(256, 10, 70));
-	}
+    @Test
+    public void testAbove255RValueCanvasColorRGBThrowsIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(256, 10, 70));
+    }
 
-	@Test
-	public void testAbove255GValueCanvasColorRGBThrowsIllegalArgumentException() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(30, 256, 70));
-	}
+    @Test
+    public void testAbove255GValueCanvasColorRGBThrowsIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(30, 256, 70));
+    }
 
-	@Test
-	public void testAbove255BValueCanvasColorRGBThrowsIllegalArgumentException() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(30, 10, 256));
-	}
+    @Test
+    public void testAbove255BValueCanvasColorRGBThrowsIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor(30, 10, 256));
+    }
 
-	// CanvasColor(hex)
-	@Test
-	public void testNormalUseCanvasColorHex() {
-		CanvasColor color = new CanvasColor("#143246");
-		Assertions.assertEquals("#143246", color.asHex());
-	}
+    // CanvasColor(hex)
+    @Test
+    public void testNormalUseCanvasColorHex() {
+        CanvasColor color = new CanvasColor("#143246", "1.0");
+        Assertions.assertEquals("#143246", color.asHex());
+    }
 
-	// CanvasColor(hex)
-	@Test
-	public void testNormalUseCanvasColorHexNoHashInput() {
-		CanvasColor color = new CanvasColor("143246");
-		Assertions.assertEquals("#143246", color.asHex());
-	}
+    // CanvasColor(hex)
+    @Test
+    public void testNormalUseCanvasColorHexNoHashInput() {
+        CanvasColor color = new CanvasColor("143246", "0.6");
+        Assertions.assertEquals("#143246", color.asHex());
+    }
 
-	@Test
-	public void testNormalUseCanvasColorHexWhite() {
-		CanvasColor color = new CanvasColor("#FFFFFF");
-		Assertions.assertEquals("#FFFFFF", color.asHex());
-	}
+    @Test
+    public void testNormalUseCanvasColorHexWhite() {
+        CanvasColor color = new CanvasColor("#FFFFFF", "0.6");
+        Assertions.assertEquals("#FFFFFF", color.asHex());
+    }
 
-	@Test
-	public void testNormalUseCanvasColorHexBlack() {
-		CanvasColor color = new CanvasColor("#000000");
-		Assertions.assertEquals("#000000", color.asHex());
-	}
+    @Test
+    public void testNormalUseCanvasColorHexBlack() {
+        CanvasColor color = new CanvasColor("#000000", "0.6");
+        Assertions.assertEquals("#000000", color.asHex());
+    }
 
-	@Test
-	public void testWrongSizeArgumentCanvasColorHexThrowsIllegalArgumentException() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor("error"));
-	}
+    @Test
+    public void testWrongSizeArgumentCanvasColorHexThrowsIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor("error", "0.5"));
+    }
 
-	@Test
-	public void testWrongTypeArgumentCanvasColorHexThrowsIllegalArgumentException() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor("1111111"));
-	}
+    @Test
+    public void testWrongTypeArgumentCanvasColorHexThrowsIllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor("1111111", ""));
+    }
 
-	@Test
-	public void testWrongTypeArgumentCanvasColorHexThrowsIllegalArgumentException2() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor("#123g43"));
-	}
+    @Test
+    public void testWrongTypeArgumentCanvasColorHexThrowsIllegalArgumentException2() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new CanvasColor("#123g43", "0.56"));
+    }
 
-	// toPaintColor()
-	@Test
-	public void testNormalUseToPaintColor() {
-		Assertions.assertEquals(Color.ALICEBLUE,
-			new CanvasColor((int) (Color.ALICEBLUE.getRed() * 255), (int) (Color.ALICEBLUE.getGreen() * 255),
-				(int) (Color.ALICEBLUE.getBlue() * 255)).toPaintColor());
-	}
+    // toPaintColor()
+    @Test
+    public void testNormalUseToPaintColor() {
+        Assertions.assertEquals(Color.ALICEBLUE,
+                new CanvasColor((int) (Color.ALICEBLUE.getRed() * 255), (int) (Color.ALICEBLUE.getGreen() * 255),
+                        (int) (Color.ALICEBLUE.getBlue() * 255), (int) (Color.ALICEBLUE.getOpacity() * 255))
+                        .toPaintColor());
+    }
 
 }
--- src/test/java/org/pinte/models/CanvasObjects/CanvasEllipseTest.java
+++ src/test/java/org/pinte/models/CanvasObjects/CanvasEllipseTest.java
@@ -11,58 +11,60 @@ import org.pinte.models.Utils.CanvasObjectParser;
  */
 public class CanvasEllipseTest {
 
-	Point2D center;
-	double radiusX, radiusY;
-	CanvasColor fillColor, strokeColor;
-	String SVGString;
+    Point2D center;
+    double radiusX, radiusY;
+    CanvasColor fillColor, strokeColor;
+    String SVGString;
 
-	void testEqual(CanvasEllipse ellipse) {
-		Assertions.assertEquals(ellipse.center, center);
-		Assertions.assertEquals(ellipse.rx, radiusX);
-		Assertions.assertEquals(ellipse.ry, radiusY);
-		Assertions.assertTrue(ellipse.fillColor.equals(fillColor));
-		Assertions.assertTrue(ellipse.strokeColor.equals(strokeColor));
-	}
+    void testEqual(CanvasEllipse ellipse) {
+        Assertions.assertEquals(ellipse.center, center);
+        Assertions.assertEquals(ellipse.rx, radiusX);
+        Assertions.assertEquals(ellipse.ry, radiusY);
+        Assertions.assertTrue(ellipse.fillColor.equals(fillColor));
+        Assertions.assertTrue(ellipse.strokeColor.equals(strokeColor));
+    }
 
-	@BeforeEach
-	public void setUp() {
-		center = new Point2D(0, 0);
-		radiusX = 3;
-		radiusY = 9;
-		fillColor = new CanvasColor(0, 255, 0);
-		strokeColor = new CanvasColor(0, 0, 255);
-	}
+    @BeforeEach
+    public void setUp() {
+        center = new Point2D(0, 0);
+        radiusX = 3;
+        radiusY = 9;
+        fillColor = new CanvasColor(0, 255, 0);
+        strokeColor = new CanvasColor(0, 0, 255);
+    }
 
-	@Test
-	public void testConstructorRadiuses() {
-		CanvasEllipse ellipse = new CanvasEllipse(center, radiusX, radiusY, fillColor, strokeColor);
-		testEqual(ellipse);
-	}
+    @Test
+    public void testConstructorRadiuses() {
+        CanvasEllipse ellipse = new CanvasEllipse(center, radiusX, radiusY, fillColor, strokeColor);
+        testEqual(ellipse);
+    }
 
-	@Test
-	public void testConstructorPoints() {
-		Point2D topLeft = new Point2D(-3, -9);
-		Point2D bottomRight = new Point2D(3, 9);
-		CanvasEllipse ellipse = new CanvasEllipse(topLeft, bottomRight, fillColor, strokeColor);
-		testEqual(ellipse);
-	}
+    @Test
+    public void testConstructorPoints() {
+        Point2D topLeft = new Point2D(-3, -9);
+        Point2D bottomRight = new Point2D(3, 9);
+        CanvasEllipse ellipse = new CanvasEllipse(topLeft, bottomRight, fillColor, strokeColor);
+        testEqual(ellipse);
+    }
 
-	@Test
-	public void testSVGString() {
-		CanvasEllipse ellipse = new CanvasEllipse(center, radiusX, radiusY, fillColor, strokeColor);
-		String svgString = ellipse.toSVG();
-		Double cx = Double.parseDouble(CanvasObjectParser.parseKeyword("cx", svgString));
-		Double cy = Double.parseDouble(CanvasObjectParser.parseKeyword("cy", svgString));
-		Double rx = Double.parseDouble(CanvasObjectParser.parseKeyword("rx", svgString));
-		Double ry = Double.parseDouble(CanvasObjectParser.parseKeyword("ry", svgString));
-		CanvasColor fill = new CanvasColor(CanvasObjectParser.parseKeyword("fill", svgString));
-		CanvasColor stroke = new CanvasColor(CanvasObjectParser.parseKeyword("stroke", svgString));
+    @Test
+    public void testSVGString() {
+        CanvasEllipse ellipse = new CanvasEllipse(center, radiusX, radiusY, fillColor, strokeColor);
+        String svgString = ellipse.toSVG();
+        Double cx = Double.parseDouble(CanvasObjectParser.parseKeyword("cx", svgString));
+        Double cy = Double.parseDouble(CanvasObjectParser.parseKeyword("cy", svgString));
+        Double rx = Double.parseDouble(CanvasObjectParser.parseKeyword("rx", svgString));
+        Double ry = Double.parseDouble(CanvasObjectParser.parseKeyword("ry", svgString));
+        CanvasColor fill = new CanvasColor(CanvasObjectParser.parseKeyword("fill", svgString),
+                CanvasObjectParser.parseKeyword("fill-opacity", svgString));
+        CanvasColor stroke = new CanvasColor(CanvasObjectParser.parseKeyword("stroke", svgString),
+                CanvasObjectParser.parseKeyword("stroke-opacity", svgString));
 
-		Assertions.assertEquals(cx, center.getX());
-		Assertions.assertEquals(cy, center.getY());
-		Assertions.assertEquals(rx, radiusX);
-		Assertions.assertEquals(ry, radiusY);
-		Assertions.assertEquals(fill.asHex(), fillColor.asHex());
-		Assertions.assertEquals(stroke.asHex(), strokeColor.asHex());
-	}
+        Assertions.assertEquals(cx, center.getX());
+        Assertions.assertEquals(cy, center.getY());
+        Assertions.assertEquals(rx, radiusX);
+        Assertions.assertEquals(ry, radiusY);
+        Assertions.assertEquals(fill.asHex(), fillColor.asHex());
+        Assertions.assertEquals(stroke.asHex(), strokeColor.asHex());
+    }
 }
