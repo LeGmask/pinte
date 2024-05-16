294f5a3e7e3bf9f88db4ca06c7b14f99d70d6e28
Louis Thevenet <louis.tvnt@gmail.com>
2024-05-16 11:59:11 +0200

    feat: add visual feedback when drawing objects

--- src/main/java/org/pinte/models/states/addEllipseState.java
+++ src/main/java/org/pinte/models/states/addEllipseState.java
@@ -5,36 +5,56 @@ import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
 import org.pinte.models.CanvasObjects.CanvasColor;
 import org.pinte.models.CanvasObjects.CanvasEllipse;
+import org.pinte.models.CanvasObjects.CanvasObject;
 
 public class addEllipseState extends State {
-	/**
-	 * Points on ellipse to be added.
-	 */
-	Point2D p1, p2;
+    /**
+     * Points on ellipse to be added.
+     */
+    Point2D p1, p2;
 
-	public addEllipseState() {
-		canvas.javafxCanvas.setOnMouseClicked(registerP1());
-	}
+    public addEllipseState() {
+        canvas.javafxCanvas.setOnMouseClicked(registerP1());
+    }
 
-	public EventHandler<MouseEvent> registerP1() {
-		return new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent e) {
-				p1 = new Point2D(e.getX(), e.getY());
-				canvas.javafxCanvas.setOnMouseClicked(registerP2());
-			}
-		};
-	}
+    public EventHandler<MouseEvent> registerP1() {
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                p1 = new Point2D(e.getX(), e.getY());
 
-	public EventHandler<MouseEvent> registerP2() {
-		return new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent e) {
-				p2 = new Point2D(e.getX(), e.getY());
-				canvas.add(new CanvasEllipse(p1, p2, new CanvasColor(0, 0, 0),
-					new CanvasColor(255, 0, 0)));
-				canvas.javafxCanvas.setOnMouseClicked(registerP1());
-			}
-		};
-	}
+                canvas.javafxCanvas.setOnMouseMoved(new EventHandler<MouseEvent>() {
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        canvas.removeGhostObject();
+
+                        CanvasObject ellipse = new CanvasEllipse(new Point2D(p1.getX(), p1.getY()),
+                                Math.abs(event.getX() - p1.getX()), Math.abs(event.getY() - p1.getY()),
+                                new CanvasColor(255, 255, 255, 0), new CanvasColor(128, 128, 0, 1));
+
+                        ellipse.isSelected = true;
+                        canvas.setGhostObject(ellipse);
+
+                    }
+                });
+
+                canvas.javafxCanvas.setOnMouseClicked(registerP2());
+            }
+        };
+    }
+
+    public EventHandler<MouseEvent> registerP2() {
+        return new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                canvas.add(new CanvasEllipse(p1, Math.abs(e.getX() - p1.getX()), Math.abs(e.getY() - p1.getY()),
+                        new CanvasColor(0, 0, 0),
+                        new CanvasColor(255, 0, 0)));
+                canvas.javafxCanvas.setOnMouseClicked(registerP1());
+                canvas.javafxCanvas.setOnMouseMoved(null);
+                canvas.removeGhostObject();
+            }
+        };
+    }
 }
