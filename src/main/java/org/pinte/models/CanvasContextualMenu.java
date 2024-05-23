e0004361e174ff553c53213f088f025ebc8f578d
Louis Thevenet <louis.tvnt@gmail.com>
2024-05-23 17:10:16 +0200

    feat: delete objects

--- src/main/java/org/pinte/models/CanvasContextualMenu.java
+++ src/main/java/org/pinte/models/CanvasContextualMenu.java
@@ -15,165 +15,190 @@ import java.util.List;
 
 public class CanvasContextualMenu {
 
-    /**
-     * Creates a context menu for the canvas
-     *
-     * @param canvas
-     * @return
-     */
-    public static EventHandler<MouseEvent> getContextualMenu(Canvas canvas) {
-        return new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent e) {
-                List<CanvasObject> pointedShapes = isOnShape(e, canvas);
-                if (!pointedShapes.isEmpty()) {
-                    shapeContextualMenu(e, canvas, pointedShapes);
-                } else {
-                    canvasContextualMenu(e, canvas);
-                }
+	/**
+	 * Creates a context menu for the canvas
+	 *
+	 * @param canvas
+	 * @return
+	 */
+	public static EventHandler<MouseEvent> getContextualMenu(Canvas canvas) {
+		return new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				List<CanvasObject> pointedShapes = isOnShape(e, canvas);
+				if (!pointedShapes.isEmpty()) {
+					shapeContextualMenu(e, canvas, pointedShapes);
+				} else {
+					canvasContextualMenu(e, canvas);
+				}
 
-            }
-        };
-    }
+			}
+		};
+	}
 
-    /**
-     * Returns the list of objects that are on the mouse position
-     *
-     * @param e
-     * @param canvas
-     * @return
-     */
-    protected static List<CanvasObject> isOnShape(MouseEvent e, Canvas canvas) {
-        List<CanvasObject> shapes = new ArrayList<CanvasObject>();
-        for (CanvasObject shape : canvas.objects) {
-            if (shape.contains(e.getX(), e.getY())) {
-                shapes.add(shape);
-            }
-        }
-        return shapes;
-    }
+	/**
+	 * Returns the list of objects that are on the mouse position
+	 *
+	 * @param e
+	 * @param canvas
+	 * @return
+	 */
+	protected static List<CanvasObject> isOnShape(MouseEvent e, Canvas canvas) {
+		List<CanvasObject> shapes = new ArrayList<CanvasObject>();
+		for (CanvasObject shape : canvas.objects) {
+			if (shape.contains(e.getX(), e.getY())) {
+				shapes.add(shape);
+			}
+		}
+		return shapes;
+	}
 
-    /**
-     * Creates the contextual menu for the shapes
-     *
-     * @param e
-     * @param canvas
-     * @param pointedShapes
-     */
-    protected static void shapeContextualMenu(MouseEvent e, Canvas canvas, List<CanvasObject> pointedShapes) {
+	/**
+	 * Creates the contextual menu for the shapes
+	 *
+	 * @param e
+	 * @param canvas
+	 * @param pointedShapes
+	 */
+	protected static void shapeContextualMenu(MouseEvent e, Canvas canvas, List<CanvasObject> pointedShapes) {
 
-        switch (e.getButton()) {
-            case PRIMARY:
-                if (e.isControlDown()) {
-                    pointedShapes.getLast().isSelected = !pointedShapes.getLast().isSelected;
-                } else {
-                    List<CanvasObject> toMove = new ArrayList<CanvasObject>();
-                    // is one is selected, move all selected. else move the most recent one
-                    boolean oneSelected = false;
+		switch (e.getButton()) {
+			case PRIMARY:
+				if (e.isControlDown()) {
+					pointedShapes.getLast().isSelected = !pointedShapes.getLast().isSelected;
+				} else {
+					List<CanvasObject> toMove = new ArrayList<CanvasObject>();
+					// is one is selected, move all selected. else move the most recent one
+					boolean oneSelected = false;
 
-                    oneSelected = !pointedShapes.isEmpty() && pointedShapes.getLast().isSelected;
+					oneSelected = !pointedShapes.isEmpty() && pointedShapes.getLast().isSelected;
 
-                    if (oneSelected) {
-                        for (CanvasObject shape : canvas.objects) {
-                            if (shape.isSelected) {
-                                toMove.add(shape);
-                            }
-                        }
-                    } else {
-                        toMove.add(pointedShapes.getLast());
+					if (oneSelected) {
+						for (CanvasObject shape : canvas.objects) {
+							if (shape.isSelected) {
+								toMove.add(shape);
+							}
+						}
+					} else {
+						toMove.add(pointedShapes.getLast());
 
-                    }
+					}
 
-                    canvas.javafxCanvas.removeEventHandler(MouseEvent.MOUSE_CLICKED, getContextualMenu(canvas));
+					canvas.javafxCanvas.removeEventHandler(MouseEvent.MOUSE_CLICKED, getContextualMenu(canvas));
 
-                    translateState translateState = new translateState(toMove, e);
-                    translateState.enterTranslateState();
+					translateState translateState = new translateState(toMove, e);
+					translateState.enterTranslateState();
 
-                }
-                break;
-            case SECONDARY:
-                final ContextMenu contextMenu = new ContextMenu();
-                contextMenu.setOnShowing(new EventHandler<WindowEvent>() {
-                    public void handle(WindowEvent e) {
-                        System.out.println("showing shape context menu for");
-                        for (CanvasObject shape : pointedShapes) {
-                            System.out.println(shape.toSVG());
-                        }
-                    }
-                });
-                contextMenu.setOnShown(new EventHandler<WindowEvent>() {
-                    public void handle(WindowEvent e) {
-                        System.out.println("shown shape context menu");
-                    }
-                });
+				}
+				break;
+			case SECONDARY:
+				final ContextMenu contextMenu = new ContextMenu();
+				contextMenu.setOnShowing(new EventHandler<WindowEvent>() {
+					public void handle(WindowEvent e) {
+						System.out.println("showing shape context menu for");
+						for (CanvasObject shape : pointedShapes) {
+							System.out.println(shape.toSVG());
+						}
+					}
+				});
+				contextMenu.setOnShown(new EventHandler<WindowEvent>() {
+					public void handle(WindowEvent e) {
+						System.out.println("shown shape context menu");
+					}
+				});
 
-                // dummy items for now
-                MenuItem item1 = new MenuItem("Copy");
-                item1.setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        System.out.println("Copied");
-                    }
-                });
-                MenuItem item2 = new MenuItem("Reshape");
-                item2.setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        System.out.println("Reshaped");
-                    }
-                });
-                contextMenu.getItems().addAll(item1, item2);
-                contextMenu.show(canvas.javafxCanvas, e.getScreenX(), e.getScreenY());
-                break;
+				// dummy items for now
+				MenuItem item1 = new MenuItem("Copy");
+				item1.setOnAction(new EventHandler<ActionEvent>() {
+					public void handle(ActionEvent e) {
+						System.out.println("Copied");
+					}
+				});
+				MenuItem item2 = new MenuItem("Reshape");
+				item2.setOnAction(new EventHandler<ActionEvent>() {
+					public void handle(ActionEvent e) {
+						System.out.println("Reshaped");
+					}
+				});
+				MenuItem item3 = new MenuItem("Delete");
+				item3.setOnAction(new EventHandler<ActionEvent>() {
+					public void handle(ActionEvent e) {
+						List<CanvasObject> selected = new ArrayList<CanvasObject>();
 
-            default:
-                break;
-        }
+						boolean oneSelected = false;
 
-    }
+						oneSelected = !pointedShapes.isEmpty() && pointedShapes.getLast().isSelected;
 
-    /**
-     * Creates the contextual menu for the canvas
-     *
-     * @param e
-     * @param canvas
-     */
-    protected static void canvasContextualMenu(MouseEvent e, Canvas canvas) {
-        switch (e.getButton()) {
-            case PRIMARY:
-                break;
-            case SECONDARY:
-                final ContextMenu contextMenu = new ContextMenu();
-                contextMenu.setOnShowing(new EventHandler<WindowEvent>() {
-                    public void handle(WindowEvent e) {
-                        System.out.println("showing canvas context menu");
-                    }
-                });
-                contextMenu.setOnShown(new EventHandler<WindowEvent>() {
-                    public void handle(WindowEvent e) {
-                        System.out.println("shown canvas context menu");
-                    }
-                });
+						if (oneSelected) {
+							for (CanvasObject shape : canvas.objects) {
+								if (shape.isSelected) {
+									selected.add(shape);
+								}
+							}
+						} else {
+							selected.add(pointedShapes.getLast());
 
-                // dummy items for now
-                MenuItem item1 = new MenuItem("Resize");
-                item1.setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        System.out.println("Resized");
-                    }
-                });
-                MenuItem item2 = new MenuItem("Save");
-                item2.setOnAction(new EventHandler<ActionEvent>() {
-                    public void handle(ActionEvent e) {
-                        System.out.println("Saved");
-                    }
-                });
-                contextMenu.getItems().addAll(item1, item2);
-                contextMenu.show(canvas.javafxCanvas, e.getScreenX(), e.getScreenY());
-                break;
+						}
 
-            default:
-                break;
-        }
+						for (CanvasObject object : selected) {
+							canvas.objects.remove(object);
+						}
+					}
+				});
+				contextMenu.getItems().addAll(item1, item2, item3);
+				contextMenu.show(canvas.javafxCanvas, e.getScreenX(), e.getScreenY());
+				break;
 
-    }
+			default:
+				break;
+		}
+
+	}
+
+	/**
+	 * Creates the contextual menu for the canvas
+	 *
+	 * @param e
+	 * @param canvas
+	 */
+	protected static void canvasContextualMenu(MouseEvent e, Canvas canvas) {
+		switch (e.getButton()) {
+			case PRIMARY:
+				break;
+			case SECONDARY:
+				final ContextMenu contextMenu = new ContextMenu();
+				contextMenu.setOnShowing(new EventHandler<WindowEvent>() {
+					public void handle(WindowEvent e) {
+						System.out.println("showing canvas context menu");
+					}
+				});
+				contextMenu.setOnShown(new EventHandler<WindowEvent>() {
+					public void handle(WindowEvent e) {
+						System.out.println("shown canvas context menu");
+					}
+				});
+
+				// dummy items for now
+				MenuItem item1 = new MenuItem("Resize");
+				item1.setOnAction(new EventHandler<ActionEvent>() {
+					public void handle(ActionEvent e) {
+						System.out.println("Resized");
+					}
+				});
+				MenuItem item2 = new MenuItem("Save");
+				item2.setOnAction(new EventHandler<ActionEvent>() {
+					public void handle(ActionEvent e) {
+						System.out.println("Saved");
+					}
+				});
+				contextMenu.getItems().addAll(item1, item2);
+				contextMenu.show(canvas.javafxCanvas, e.getScreenX(), e.getScreenY());
+				break;
+
+			default:
+				break;
+		}
+
+	}
 
 }
