ed579219f0662fa935379535d39756f0a96553c6
Louis Thevenet <louis.tvnt@gmail.com>
2024-05-24 17:51:39 +0200

    fix: polygon color on creation

--- src/main/java/org/pinte/models/states/addPolygonState.java
+++ src/main/java/org/pinte/models/states/addPolygonState.java
@@ -36,8 +36,8 @@ public class addPolygonState extends State {
 						canvas.removeGhostObject();
 						Point2D p = new Point2D(event.getX(), event.getY());
 						CanvasObject polygon = new CanvasLine(points.getFirst(), p,
-													new CanvasColor(0, 0, 0, 0),
-													new CanvasColor(128, 128, 0, 1));
+								new CanvasColor(0, 0, 0, 0),
+								new CanvasColor(128, 128, 0, 1));
 						polygon.isSelected = true;
 						canvas.setGhostObject(polygon);
 
@@ -62,8 +62,8 @@ public class addPolygonState extends State {
 						ArrayList<Point2D> tmpList = new ArrayList<Point2D>(points);
 						tmpList.add(p);
 						CanvasObject polygon = new CanvasPolygon(tmpList.toArray(new Point2D[0]),
-													new CanvasColor(0, 0, 0, 0),
-													new CanvasColor(128, 128, 0, 1));
+								new CanvasColor(0, 0, 0, 0),
+								new CanvasColor(128, 128, 0, 1));
 
 						polygon.isSelected = true;
 						canvas.setGhostObject(polygon);
@@ -83,12 +83,13 @@ public class addPolygonState extends State {
 	}
 
 	public void registerPolygon() {
-		CanvasObject polygon = points.size() == 2 ? new CanvasLine(points.getFirst(), points.getLast(),
-														new CanvasColor(0, 0, 0, 0),
-														new CanvasColor(128, 128, 0, 255))
-												  : new CanvasPolygon((points.toArray(new Point2D[0])),
-														new CanvasColor(0, 0, 0),
-														new CanvasColor(255, 0, 0));
+		CanvasObject polygon = points.size() == 2
+				? new CanvasLine(points.getFirst(), points.getLast(), canvas.getCopyColorSelect(),
+						canvas.getCopyColorSelect())
+				: new CanvasPolygon((points.toArray(new Point2D[0])),
+
+						canvas.getCopyColorSelect(), canvas.getCopyColorSelect());
+
 		canvas.add(polygon);
 	}
 }
